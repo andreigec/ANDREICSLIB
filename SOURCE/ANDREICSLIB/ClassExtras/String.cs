93200bacb5a8970ef4143ac395b37d8e72970826
andrei <andreigec@hotmail.com>
2014-06-26 13:43:28 +1000

    md5 of string

--- SOURCE/ANDREICSLIB/ClassExtras/String.cs
+++ SOURCE/ANDREICSLIB/ClassExtras/String.cs
@@ -2,11 +2,42 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ANDREICSLIB
 {
     public static class StringExtras
     {
+        public static string GetMD5OfString(String s)
+        {
+            using (var md5Hash = MD5.Create())
+            {
+                var hash = GetMd5Hash(md5Hash, s);
+                return hash;
+            }
+        }
+
+        public static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            // Convert the input string to a byte array and compute the hash. 
+            var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes 
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data  
+            // and format each one as a hexadecimal string. 
+            for (var i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(i.ToString("x2"));
+            }
+
+            // Return the hexadecimal string. 
+            return sBuilder.ToString();
+        }
+
         public static string[] SplitString(String instr, String split, bool removeempty = true)
         {
             var s = new string[1];
