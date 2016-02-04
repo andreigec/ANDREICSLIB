e3e8ff95560a9fa737708ea2459a5f7d85da5061
Andrei Gec <andreigec@hotmail.com>
2016-02-04 15:52:32 +1100

    fix time str

--- Helpers/TimeUpdates.cs
+++ Helpers/TimeUpdates.cs
@@ -22,30 +22,30 @@ namespace ANDREICSLIB.Helpers
             public Func<TimeSpan, double> Get { get; }
             public double Limit { get; }
 
-            public static TimeR MS = new TimeR("Millisecond", s => s.TotalMilliseconds, 1001);
+            public static TimeR Ms = new TimeR("Millisecond", s => s.TotalMilliseconds, 1001);
             public static TimeR S = new TimeR("Second", s => s.TotalSeconds, 61);
             public static TimeR M = new TimeR("Minute", s => s.TotalMinutes, 61);
             public static TimeR H = new TimeR("Hour", s => s.TotalHours, 24);
             public static TimeR D = new TimeR("Day", s => s.TotalDays, 265);
 
-            public static List<TimeR> list = new List<TimeR>() { MS, S, M, H, D };
+            public static List<TimeR> Times = new List<TimeR>() { Ms, S, M, H, D };
         }
 
         public static string TimeInWords(TimeSpan ts)
         {
-            foreach (var l in TimeR.list)
+            foreach (var l in TimeR.Times)
             {
                 var val = l.Get(ts);
                 if (val < l.Limit)
                     return Pluralise(l.Name, val);
             }
 
-            return Pluralise(TimeR.MS.Name, TimeR.MS.Get(ts));
+            return Pluralise(TimeR.Ms.Name, TimeR.Ms.Get(ts));
         }
 
         public static string Pluralise(string word, double val)
         {
-            string ret = word;
+            string ret = val.ToString("N") + " " + word;
             if (val != 1)
             {
                 if (ret.EndsWith("s") == false)
